ac0f0c2fdeedbfad00c704019a00052892730852
Nataniel Mendes <nataniel.mendes@sfappworks.com>
2021-10-19 17:40:09 -0300

    Calculating X percentage of Y in percentage-calculation page

--- src/pages/percentage-calculation.tsx
+++ src/pages/percentage-calculation.tsx
@@ -11,16 +11,40 @@ type DataProps = {
   }
 }
 
-const UsingTypescript: React.FC<PageProps<DataProps>> = ({ data, path }) => (
-  <Layout>
-    <Seo title="Percentage Calculation" />
-    <h1>Percentage Calculation</h1>
-    <p>
-      5 is 5% of 100
-    </p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+const UsingTypescript: React.FC<PageProps<DataProps>> = ({ data, path }) => {
+  const [firstPercentageValue, setFirstPercentageValue] = React.useState(5);
+  const [secondPercentageValue, setSecondPercentageValue] = React.useState(100);
+  const [result, setResult] = React.useState(5);
+
+  React.useEffect(() => {
+    updateResult();
+  })
+
+  const handleFirstPercentageValue = event => {
+    setFirstPercentageValue(event.target.value);
+  }
+  
+  const handleSecondPercentageValue = event => {
+    setSecondPercentageValue(event.target.value);
+  }
+  
+  const updateResult = () => {
+    setResult(firstPercentageValue * 100 / secondPercentageValue);
+  }
+  
+  return (
+    <Layout>
+      <Seo title="Percentage Calculation" />
+      <h1>Percentage Calculation</h1>
+      <p>
+        <input id="first-value" type="text" value={firstPercentageValue} onChange={handleFirstPercentageValue}></input>
+        &nbsp;is {result}% of&nbsp;
+        <input id="second-value" type="text" value={secondPercentageValue} onChange={handleSecondPercentageValue}></input>
+      </p>
+      <Link to="/">Go back to the homepage</Link>
+    </Layout>
+  )
+}
 
 export default UsingTypescript
 
