799ddf5223d6d7750e47f672ebc641b0860a4882
Nataniel Mendes <natanielmendes@hotmail.com>
2023-03-07 15:52:04 -0300

    Eliminating duplicated content

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,48 +32,6 @@ const IndexPage = () => (
         </p>
       </div>
     </div>
-    <div className="home__container">
-      <div className="home__profile">
-        <OutboundLink href="https://app.daily.dev/natanielmendes" alt="Link to Dev.to Nataniel Carvalho's profile">
-          <StaticImage
-            src="https://api.daily.dev/devcards/f9aec4e8d42049ddbddc03f35ee67424.png?r=ygs"
-            width={300}
-            formats={["auto", "webp", "avif", "png"]}
-            alt="Nataniel Carvalho's Dev Card"
-            style={{ marginBottom: `1.45rem` }}
-          />
-        </OutboundLink>
-      </div>
-      <div className="home__useful-resources">
-        <p>Useful Resources to daily persistent worries:</p>
-        <p>
-          <Link to="/future-useful-resource/">Go to future useful resource</Link> <br />
-          <Link to="/percentage-calculation/">Go to "Percentage Calculation"</Link> <br />
-          <OutboundLink href="https://natanielmendes.github.io/compound-interest-calculator/">Go to "Compound Interest Calculator"</OutboundLink>
-        </p>
-      </div>
-    </div>
-    <div className="home__container">
-      <div className="home__profile">
-        <OutboundLink href="https://app.daily.dev/natanielmendes" alt="Link to Dev.to Nataniel Carvalho's profile">
-          <StaticImage
-            src="https://api.daily.dev/devcards/f9aec4e8d42049ddbddc03f35ee67424.png?r=ygs"
-            width={300}
-            formats={["auto", "webp", "avif", "png"]}
-            alt="Nataniel Carvalho's Dev Card"
-            style={{ marginBottom: `1.45rem` }}
-          />
-        </OutboundLink>
-      </div>
-      <div className="home__useful-resources">
-        <p>Useful Resources to daily persistent worries:</p>
-        <p>
-          <Link to="/future-useful-resource/">Go to future useful resource</Link> <br />
-          <Link to="/percentage-calculation/">Go to "Percentage Calculation"</Link> <br />
-          <OutboundLink href="https://natanielmendes.github.io/compound-interest-calculator/">Go to "Compound Interest Calculator"</OutboundLink>
-        </p>
-      </div>
-    </div>
   </Layout>
 )
 
